bcc8972c2eb4d2f1299ae993b1e5f9ae0c02a4ed
Igor <klyuzhevigor@gmail.com>
2022-02-21 09:11:00 +0300

    LAB2: [new] Added partials for header and footer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', '/views/partials'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
