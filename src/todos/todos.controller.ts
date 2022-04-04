3fe2ea03d9f200a389da440f092db5a189e257a6
Igor <klyuzhevigor@gmail.com>
2022-04-04 23:42:17 +0300

    LAB4: [new] Added todos controller and service

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,47 @@
+import { Get, Post, Delete, Param, Controller, NotImplementedException, Body } from "@nestjs/common";
+import TodosService from './todos.service';
+import { User } from '../decorators/user.decorator'
+import { ToDo as ToDoModel } from '@prisma/client';
+
+import {
+  ApiBearerAuth, ApiTags,
+} from '@nestjs/swagger';
+
+@ApiBearerAuth()
+@ApiTags('todos')
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get(':id')
+  async getTodoById(@User('id') userId: number, @Param('id') todoId: string): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  @Get()
+  async getAllTodos(@User('id') userId: number): Promise<ToDoModel[]> {
+    throw new NotImplementedException();
+  }
+
+  @Post('add')
+  async addTodo(@User('id') userId: number, @Body() todoData: { title: string; description?: string }): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  @Post('update/:id')
+  async updateTodo(@User('id') userId: number,
+                   @Param('id') todoId: string,
+                   @Body() todoData: { title?: string; description?: string }): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  @Post('toggle/:id')
+  async toggleState(@User('id') userId: number, @Param('id') todoId: string): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  @Delete(':id')
+  async removeTodo(@User('id') userId: number,  @Param('id') todoId: string): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+}
