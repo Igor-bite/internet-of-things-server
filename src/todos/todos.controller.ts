fa49f822c9ff5d1917a41d3fe12a87955133414f
Igor <klyuzhevigor@gmail.com>
2022-04-05 00:14:36 +0300

    LAB4: [feat] Filled todos controller and service

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,8 +1,7 @@
-import { Get, Post, Delete, Param, Controller, NotImplementedException, Body } from "@nestjs/common";
+import { Get, Post, Delete, Param, Controller, Body } from "@nestjs/common";
 import TodosService from './todos.service';
 import { User } from '../decorators/user.decorator'
-import { ToDo as ToDoModel } from '@prisma/client';
-
+import { ToDo as ToDoModel, TodoState } from '@prisma/client';
 import {
   ApiBearerAuth, ApiTags,
 } from '@nestjs/swagger';
@@ -14,34 +13,51 @@ export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Get()
-  async getAllTodos(@User('id') userId: number): Promise<ToDoModel[]> {
-    throw new NotImplementedException();
+  async getAllTodos(
+    @User('id') userId: number
+  ): Promise<ToDoModel[]> {
+    return this.todosService.getAllTodos(userId);
   }
 
   @Get(':id')
-  async getTodoById(@User('id') userId: number, @Param('id') todoId: string): Promise<ToDoModel> {
-    throw new NotImplementedException();
+  async getTodoById(
+    @User('id') userId: number,
+    @Param('id') todoId: string
+  ): Promise<ToDoModel> {
+    return this.todosService.getTodoById(userId, todoId);
   }
 
   @Post('add')
-  async addTodo(@User('id') userId: number, @Body() todoData: { title: string; description?: string }): Promise<ToDoModel> {
-    throw new NotImplementedException();
+  async addTodo(
+    @User('id') userId: number,
+    @Body() todoData: { title: string; description?: string }
+  ): Promise<ToDoModel> {
+    return this.todosService.addTodo(userId, todoData);
   }
 
   @Post('update/:id')
-  async updateTodo(@User('id') userId: number,
-                   @Param('id') todoId: string,
-                   @Body() todoData: { title?: string; description?: string }): Promise<ToDoModel> {
-    throw new NotImplementedException();
+  async updateTodo(
+    @User('id') userId: number,
+    @Param('id') todoId: string,
+    @Body() todoData: { title?: string; description?: string }
+  ): Promise<ToDoModel> {
+    return this.todosService.updateTodo(userId, todoId, todoData);
   }
 
-  @Post('changeState/:id:state')
-  async changeState(@User('id') userId: number, @Param('id') todoId: string, @Param('state') state: string): Promise<ToDoModel> {
-    throw new NotImplementedException();
+  @Post('changeState/:id&:state')
+  async changeState(
+    @User('id') userId: number,
+    @Param('id') todoId: string,
+    @Param('state') state: TodoState
+  ): Promise<ToDoModel> {
+    return this.todosService.changeState(userId, todoId, state);
   }
 
   @Delete(':id')
-  async removeTodo(@User('id') userId: number,  @Param('id') todoId: string): Promise<ToDoModel> {
-    throw new NotImplementedException();
+  async removeTodo(
+    @User('id') userId: number,
+    @Param('id') todoId: string
+  ): Promise<ToDoModel> {
+    return this.todosService.removeTodo(userId, todoId);
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from "@nestjs/common";
+import { ToDo as ToDoModel, TodoState} from '@prisma/client';
 
 @Injectable()
-export default class TodosService {}
+export default class TodosService {
+  getAllTodos(userId: number): Promise<ToDoModel[]> {
+    throw new NotImplementedException();
+  }
+
+  getTodoById(userId: number, todoId: string): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  addTodo(userId: number, todoData: { title: string; description?: string }): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  updateTodo(userId: number, todoId: string,
+                   todoData: { title?: string; description?: string }): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  changeState(userId: number, todoId: string, state: TodoState): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+
+  removeTodo(userId: number, todoId: string): Promise<ToDoModel> {
+    throw new NotImplementedException();
+  }
+}
