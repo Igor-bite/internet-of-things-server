7843befe7487fa2020026a2b70679e018db49865
Igor <klyuzhevigor@gmail.com>
2022-04-04 23:59:22 +0300

    LAB4: [new] Added generating swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import * as hbs from 'hbs';
 import { AppModule } from './app.module';
 import { ResponseTimeInterceptor } from "./interceptors/timecalc.interceptor";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -18,6 +19,15 @@ async function bootstrap() {
   hbs.registerHelper('isdefined', function (value) {
     return value !== undefined;
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('IoT Server API')
+    .setDescription('API for controlling your microcontrollers through internet')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
