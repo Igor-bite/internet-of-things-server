6ecb8c8b1d8e4be7371dab0b8dad8b3249c1991a
Igor <klyuzhevigor@gmail.com>
2022-04-04 23:31:31 +0300

    LAB4: [new] Added needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ProjectsModule } from "./projects/projects.module";
 import { PricesModule } from "./prices/prices.module";
 import { ApiModule } from "./api/api.module";
+import { TodosModule } from './todos/todos.module';
+import { ControlsModule } from './controls/controls.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -17,9 +20,17 @@ import { ApiModule } from "./api/api.module";
                 path: 'api',
                 module: ApiModule,
               }]
-            )
+            ),
+            TodosModule,
+            ControlsModule,
+            NewsModule
   ],
   controllers: [AppController],
   providers: [AppService],
+  exports: [ProjectsModule,
+            PricesModule,
+            TodosModule,
+            ControlsModule,
+            NewsModule]
 })
 export class AppModule {}
