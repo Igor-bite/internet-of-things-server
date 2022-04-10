87003e010bff27970b95d2b78efacd60574510d2
Igor <klyuzhevigor@gmail.com>
2022-04-10 18:20:27 +0300

    LAB5: [new] Getting displays from database

--- src/displays/displays.controller.ts
+++ src/displays/displays.controller.ts
@@ -9,17 +9,18 @@ import { ApiBearerAuth, ApiOkResponse, ApiResponse, ApiTags } from "@nestjs/swag
 @ApiTags('displays')
 @Controller('displays')
 export default class DisplaysController {
-  constructor(private readonly todosService: DisplaysService) {}
+  constructor(private readonly displaysService: DisplaysService) {}
 
-  @Get()
+  @Get('in:projectId')
   @ApiOkResponse({ description: 'Returned all displays for user' })
   @ApiResponse({ status: 204, description: 'No displays yet' })
   @ApiResponse({ status: 304, description: 'No changes' })
   @ApiResponse({ status: 401, description: 'No authorization' })
   async getAllDisplays(
-    @User('id') userId: number
+    @User('id') userId: number,
+    @Param('projectId') projectId: number
   ) {
-    return await this.todosService.getAllDisplays(userId);
+    return await this.displaysService.getAllDisplays(userId, projectId);
   }
 
   @Get(':id')
@@ -31,7 +32,7 @@ export default class DisplaysController {
     @User('id') userId: number,
     @Param('id') displayId: number
   ) {
-    return await this.todosService.getDisplayById(userId, displayId);
+    return await this.displaysService.getDisplayById(userId, displayId);
   }
 
   @Post()
@@ -42,7 +43,7 @@ export default class DisplaysController {
     @User('id') userId: number,
     @Body() displayData: CreateDisplayDto
   ) {
-    return await this.todosService.addDisplay(userId, displayData);
+    return await this.displaysService.addDisplay(userId, displayData);
   }
 
   @Put(':id')
@@ -56,7 +57,7 @@ export default class DisplaysController {
     @Param('id') displayId: number,
     @Body() displayData: UpdateDisplayDto
   ) {
-    return await this.todosService.updateDisplay(userId, displayId, displayData);
+    return await this.displaysService.updateDisplay(userId, displayId, displayData);
   }
 
   @Delete(':id')
@@ -67,6 +68,6 @@ export default class DisplaysController {
     @User('id') userId: number,
     @Param('id') displayId: number
   ) {
-    return await this.todosService.removeDisplay(userId, displayId);
+    return await this.displaysService.removeDisplay(userId, displayId);
   }
 }
--- src/displays/displays.service.ts
+++ src/displays/displays.service.ts
@@ -1,27 +1,32 @@
-import { Injectable, NotImplementedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { Display } from '@prisma/client';
 import CreateDisplayDto from "./dto/createDisplay.dto";
 import UpdateDisplayDto from "./dto/updateDisplay.dto";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export default class DisplaysService {
-  getAllDisplays(userId: number): Promise<Display[]> {
-    throw new NotImplementedException();
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async getAllDisplays(userId: number, projectId: number): Promise<Display[]> {
+    return await this.prisma.display.findMany({ where: { projectId: Number(projectId) } });
   }
 
-  getDisplayById(userId: number, displayId: number): Promise<Display> {
-    throw new NotImplementedException();
+  async getDisplayById(userId: number, displayId: number): Promise<Display> {
+    return await this.prisma.display.findFirst({ where: { id: Number(displayId) }});
   }
 
-  addDisplay(userId: number, displayData: CreateDisplayDto): Promise<Display> {
-    throw new NotImplementedException();
+  async addDisplay(userId: number, displayData: CreateDisplayDto): Promise<Display> {
+    return await this.prisma.display.create({ data: displayData });
   }
 
-  updateDisplay(userId: number, displayId: number, displayData: UpdateDisplayDto): Promise<Display> {
-    throw new NotImplementedException();
+  async updateDisplay(userId: number, displayId: number, displayData: UpdateDisplayDto): Promise<Display> {
+    return await this.prisma.display.update({ where: { id: Number(displayId) }, data: displayData });
   }
 
-  removeDisplay(userId: number, displayId: number): Promise<Display> {
-    throw new NotImplementedException();
+  async removeDisplay(userId: number, displayId: number): Promise<Display> {
+    return await this.prisma.display.delete({ where: { id: Number(displayId) } });
   }
 }
